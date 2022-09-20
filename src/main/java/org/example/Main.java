32fc4647f83da34a29de92862e543cdabb1e9067
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-09-20 09:19:13 +0530

    Release version-1.0.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Local chat console application");
+        System.out.println("Local chat console application V1.0.0");
     }
 }
