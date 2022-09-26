66ea364135ce19765fb67c1173d080627efe0e64
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-09-26 20:08:47 +0530

    Fixed Server module main error

--- Server/src/main/java/org/example/Main.java
+++ Server/src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.out.println("Need [Port] [Server capacity]");
+            return;
         }
 
         var port = Integer.parseInt(args[0]);
