cf708f9087c489e8872e8003d7940ffd1eb0926c
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-10-01 20:45:07 +0530

    Added new SET Command

--- Server/src/main/java/org/example/Server/Writer/Commands/SETCommand.java
+++ Server/src/main/java/org/example/Server/Writer/Commands/SETCommand.java
@@ -1,17 +1,49 @@
 package org.example.Server.Writer.Commands;
 
+import org.example.Server.Main.Server;
 import org.example.Server.Writer.Commands.Interfaces.InputCommand;
 
 public class SETCommand implements InputCommand {
+
+    private final Server server;
+
+    public SETCommand(Server server) {
+        this.server = server;
+    }
+
     @Override
     public void execute(String input) {
         if (input.isEmpty()) {
             System.out.println("Invalid SET Command syntax!");
-            System.out.println("Type \"help\" for help!");
+            showHelp();
             return;
         }
 
         var filteredInput = input.trim().split(" ", 2);
-        var option = filteredInput[0].trim().toLowerCase();
+        var property = filteredInput[0].trim().toLowerCase();
+        input = input.replace(property, "");
+
+        switch (property) {
+            case "sc" -> setServerCapacity(Integer.parseInt(input));
+            case "help" -> showDetailedHelp();
+            default -> showHelp();
+        }
+    }
+
+    private void setServerCapacity(int serverCapacity) {
+        server.getServerCapacity().set(serverCapacity);
+    }
+
+    private void showDetailedHelp() {
+        System.out.println("""
+                ------------------------------
+                Syntax: set [property] [value]
+                Example: set sc 5
+                ------------------------------
+                """);
+    }
+
+    private void showHelp() {
+        System.out.println("Type \"help\" for help!");
     }
 }
