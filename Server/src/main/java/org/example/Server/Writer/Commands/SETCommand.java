bbb3cd204a4e14a604ec297ba04ad13ba715ec38
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-10-03 10:37:56 +0530

    Refactored and solved edge cases in SET command

--- Server/src/main/java/org/example/Server/Writer/Commands/SETCommand.java
+++ Server/src/main/java/org/example/Server/Writer/Commands/SETCommand.java
@@ -24,14 +24,47 @@ public class SETCommand implements InputCommand {
         input = input.replace(property, "");
 
         switch (property) {
-            case "sc" -> setServerCapacity(Integer.parseInt(input.trim()));
+            case "sc" -> setServerCapacity(getScValue(input));
             case "help" -> showDetailedHelp();
             default -> showHelp();
         }
     }
 
-    private void setServerCapacity(int serverCapacity) {
-        server.getServerCapacity().set(serverCapacity);
+    private int getScValue(String input) {
+        var trimmedInput = input.trim();
+        if (trimmedInput.isEmpty()) {
+            System.out.println("Value cannot be empty");
+            return -1;
+        }
+
+        var scValue = 0;
+        try {
+            scValue = Integer.parseInt(trimmedInput);
+        } catch (Exception ex) {
+            System.out.println("Value cannot be string");
+            return -1;
+        }
+
+        return scValue;
+    }
+
+    private void setServerCapacity(int scValue) {
+        if (scValue < 0) {
+            System.out.println("Cannot set the value");
+            return;
+        }
+
+        var scObj = server.getServerCapacity();
+        var prevSc = scObj.get();
+        if (prevSc == scValue) {
+            System.out.println("Value cannot be same");
+            return;
+        }
+
+        scObj.set(scValue);
+        System.out.println("Changed server capacity " + prevSc + " -> " + scValue);
+        if (scValue < prevSc)
+            System.out.println("Connected users won't be affected by this change");
     }
 
     private void showDetailedHelp() {
