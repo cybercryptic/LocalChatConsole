928623ccc72f639845f4fb9899dd535300e1960d
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-10-03 11:13:37 +0530

    Added detailed explanation on help message

--- Server/src/main/java/org/example/Server/Writer/Commands/HelpCommand.java
+++ Server/src/main/java/org/example/Server/Writer/Commands/HelpCommand.java
@@ -6,8 +6,16 @@ public class HelpCommand implements Command {
 
     public void execute() {
         System.out.println("""
-                usr [id] [message] or
-                usr [message]
+                Explanation of commands
+                -----------------------
+                usr - To send or broadcast message
+                set - To set properties of server
+                cmd - To execute server commands
+                help - To print this message & exit
+                
+                Syntax of commands
+                ------------------
+                usr [id] [message] or usr [message]
                 set [property] [value]
                 cmd [command]
                 help [Print help message & exit]
