db5b725b25f446be749cda3e0e6e14e5d3d77596
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-10-03 20:03:33 +0530

    Refactored the code

--- Client/src/main/java/org/example/Client/Client.java
+++ Client/src/main/java/org/example/Client/Client.java
@@ -10,28 +10,33 @@ public class Client {
     private final AtomicBoolean session = new AtomicBoolean();
     public final ClientReceiver receiver;
     public final ClientWriter writer;
+    public static final Console console = new Console();
 
     public Client(String host, int port) throws IOException {
         setServer(host, port);
-        receiver = new ClientReceiver(this);
+        receiver = new ClientReceiver(this, new ReceiverCommandCenter(this));
         writer = new ClientWriter(this);
         start();
     }
 
     private void start() throws IOException {
-        if (!receiver.isRequestAccepted()) {
+        if (!receiver.isRequestAccepted())
             System.out.println("Server might be busy!! Cannot connect.\nTry Again later...");
-            return;
-        }
+
         session.set(true);
         System.out.println("Connected to server successfully");
     }
 
     public void stop() throws IOException {
-        session.set(false);
         socket.close();
 
         System.out.println("Closed successfully");
+    }
+
+    private void setServer(String host, int port) {
+        System.out.println("Connecting to Server host: " + host + " Port: " + port);
+        System.out.println("CTRL + C to stop");
+        socket = connectToServerWith(host, port);
     }
 
     private Socket connectToServerWith(String host, int port) {
@@ -55,12 +60,6 @@ public class Client {
         } catch (InterruptedException ex) {
             throw new RuntimeException(ex);
         }
-    }
-
-    private void setServer(String host, int port) {
-        System.out.println("Connecting to Server host: " + host + " Port: " + port);
-        System.out.println("CTRL + C to stop");
-        socket = connectToServerWith(host, port);
     }
 
     public Socket getSocket() {
